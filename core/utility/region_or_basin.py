9343d36be6737a9b5a8ae5c0a5da76a0cfa7b4a9
nyenah <84685025+nyenah@users.noreply.github.com>
2024-02-19 09:43:10 +0100

    Add files via upload

--- core/utility/region_or_basin.py
+++ core/utility/region_or_basin.py
@@ -1,72 +1,81 @@
-import geopandas as gpd
-
-import regionmask
+import numpy as np
+import pandas as pd 
+import xarray as xr
 
 """Select Region or Basin."""
 
 class Select_region:
     """Select region or basin of interest based on outflow station."""
-    def __init__(self, run_region, mask_watergap, super_basins, stations):
+    def __init__(self, run_region,  arc_id,  stations, lat_lon_arcid, inflow_cell):
         self.region = 0
         if run_region is True: 
-            # Specify the latitude and longitude points using list comprehensions
-            outflow_lat = stations["Lat"].values  # Replace with actual latitude
-            outflow_lon = stations["Lon"].values # Replace with actual longitude
-    
-            # Create a GeoDataFrame with the outflow points
-            outflow_gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(outflow_lon, outflow_lat))
-            
-            # Spatial query to find the basins for the outflow points
-            selected_super_basins = super_basins[super_basins.intersects(outflow_gdf.unary_union)]
-            
-            # Use regionmask to create the mask
-            mask = regionmask.mask_geopandas(selected_super_basins,  mask_watergap)
+
+            # check if arc_id are present 
+            if np.nansum(stations["station_arcid"].values)==0:
+                lat_lon_arcid = lat_lon_arcid.round(2) # round data to 2 decimal place (eg. -46.7499999999999 = -46.75)
+                
+                # Example lists of latitude and longitude
+                latitudes = stations["lat"].values.tolist()  
+                longitudes = stations["lon"].values.tolist()
+                
+                # Create an empty list to store results
+                selected_arcids = []
+                
+                # Iterate through the lists of latitude and longitude
+                for lat, lon in zip(latitudes, longitudes):
+                    # Select the arcid for the current latitude and longitude
+                    selected_row =  \
+                        lat_lon_arcid[(lat_lon_arcid['Lon'] == lon) & (lat_lon_arcid['Lat'] == lat)]['ArcID']
                     
-            self.region = mask.values*0
+                    # Check if there is a match
+                    if not selected_row.empty:
+                        selected_arcids.append(selected_row.iloc[0])
+                    else:
+                        selected_arcids.append(None)
+                
+                # Print the results
+                for i, (lat, lon) in enumerate(zip(latitudes, longitudes)):
+                    arcid = selected_arcids[i]
+                    if arcid is not None:
+                       pass
+                    else:
+                        print(f"No matching arcid found for latitude {lat} and longitude {lon}")
+
+            else:
+                selected_arcids =  stations["station_arcid"].values.tolist()
+                
+                
+            # get upstream cell data 
+            inflow_cell.set_index('Arc_ID', inplace=True)
+            upstream_cells  = selected_arcids.copy()
+            all_upstream = Select_region.get_all_upstream_cells_arcid(selected_arcids, inflow_cell, upstream_cells)
+
+            # create a 2d mask for the selected basin 
+            mask = arc_id.isin(all_upstream)
+            self.region = np.where(mask==True, 0,np.nan) 
+
         else:
-            self.region = mask_watergap.values*0
+            self.region = arc_id * 0
         
         
-
-# =============================================================================
-# To be added       
-# =============================================================================
-# import xarray as xr      
-# super_basins_shapefile = 'Z:/projects/ReWaterGAP/rewatergap/input_data/static_input/super_basins/WLM_superbasins_names_outflowcellcoords_upstreamarea.shp'
-# mask_watergap_path = 'Z:/projects/ReWaterGAP/rewatergap/input_data/static_input/cell_area.nc'
-
-# super_basins = gpd.read_file(super_basins_shapefile)
-# mask_watergap = xr.open_dataarray(mask_watergap_path, decode_times=False)
-
-# # Specify the latitude and longitude points using list comprehensions
-# outflow_lat = [0.25, ]  # Replace with actual latitude
-# outflow_lon = [-50.25, ]  # Replace with actual longitude
-
-# # Create a GeoDataFrame with the outflow points
-# outflow_gdf = gpd.GeoDataFrame(geometry=gpd.points_from_xy(outflow_lon, outflow_lat))
-
-# # Spatial query to find the basins for the outflow points
-# selected_basins = super_basins.intersects(outflow_gdf.unary_union)
+    @staticmethod
+    def get_all_upstream_cells_arcid(arcid_list, inflow_cell, upstream_cells):
+        temp_upstream_cell =[]
+        for i in arcid_list:
+            next_cells = inflow_cell.loc[i][inflow_cell.loc[i]>0]
+            temp_upstream_cell.extend(next_cells)
+        upstream_cells.extend(temp_upstream_cell)
+        if len(temp_upstream_cell) != 0 :
+            Select_region.get_all_upstream_cells_arcid(temp_upstream_cell, inflow_cell, upstream_cells)
     
-
-# # basin_shapefile_path = 'Z:/projects/ReWaterGAP/rewatergap/input_data/static_input/WaterGAP22e_cal_bas/WaterGAP22e_cal_bas.shp'
-# # basins_cal = gpd.read_file(basin_shapefile_path)
+        return upstream_cells
 
 
-# # Print information about the selected basins
-# if not selected_basins.empty:
-#     print("Selected Basins Information:")
-#     print(selected_basins)
-# else:
-#     print("No basins found for the given outflow points.")
 
-# # Optionally, you can plot the results
-# import matplotlib.pyplot as plt
 
-# ax = selected_basins.plot(edgecolor='black', figsize=(10, 10))
-# outflow_gdf.plot(ax=ax, color='red', markersize=50)
-# plt.title('Basins with Outflow Points')
-# plt.show()
 
+
+
+    
 
 
