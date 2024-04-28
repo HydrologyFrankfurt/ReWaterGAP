7ad6d5091636bd81e28ebdfe2bcd7faabbd46557
nyenah <84685025+nyenah@users.noreply.github.com>
2024-04-28 17:35:35 +0200

    Add files via upload

---
+++ view/baseunit_aggregation .py
@@ -0,0 +1,11 @@
+
+
+
+def aggregation(self, cell_area, contfrac, month_daily_aggr):
+
+
+make_mean = ["netrad", "lai-total",  "snowcover_frac"]
+   
+make_mean = ["land_area_fraction", "river_velocity"]
+                
+
