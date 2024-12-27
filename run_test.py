7849e75e7743720a36f0098744094bf3072015c6
nyenah <84685025+nyenah@users.noreply.github.com>
2024-12-27 15:39:01 +0100

    Add files via upload

--- run_test.py
+++ run_test.py
@@ -2,6 +2,7 @@
 """run all test."""
 
 import unittest
+import sys
 
 test_loader = unittest.TestLoader()
 test_suite = unittest.TestSuite()
@@ -9,5 +10,12 @@ test_suite = unittest.TestSuite()
 all_tests = test_loader.discover('.', pattern='test_*.py')
 test_suite.addTests(all_tests)
 
+
 runner = unittest.TextTestRunner(verbosity=2)
 result = runner.run(test_suite)
+
+# If any test failed, exit with code 1 (indicating failure)
+if result.wasSuccessful():
+    sys.exit(0)  # Exit with success code
+else:
+    sys.exit(1)  # Exit with failure code to indicate failure
