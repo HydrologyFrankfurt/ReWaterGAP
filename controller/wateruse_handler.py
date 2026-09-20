46c5a1b5eaed05f8be6699b10460d5d1ff8ca84f
nyenah <84685025+nyenah@users.noreply.github.com>
2026-09-20 20:04:41 +0200

    Update wateruse_handler.py

--- controller/wateruse_handler.py
+++ controller/wateruse_handler.py
@@ -121,7 +121,7 @@ class Wateruse:
                     xr.open_dataset(glwdunits_path, decode_times=False)
                 self.glwdunits = glwdunits.glwdunits.values
 
-            except OSError as error: #FileNotFoundError as error:
+            except OSError as error: #FileNotFoundError is also included
                 log.config_logger(logging.ERROR, modname, f'Water use data '
                                   f'not found \n{error}', args.debug)
                 sys.exit()  # dont run code if file does not exist
